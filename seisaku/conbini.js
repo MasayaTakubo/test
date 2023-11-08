e639214caa256e01442ae7893806fd4500b5fc45
masaya <mt23304012@ga.ttc.ac.jp>
2023-11-08 13:43:49 +0900

    [Add] seisaku

---
+++ seisaku/conbini.js
@@ -0,0 +1,34 @@
+/*const header = document.querySelector('header');
+
+
+// スクロール時のイベントリスナーを追加します。
+window.addEventListener('scroll', () => {
+  // スクロール位置を取得します。
+  const scrollY = window.scrollY;
+
+  // ヘッダーが特定のスクロール位置よりも下にスクロールした場合、
+  // ヘッダーを非表示にします。
+  if (scrollY > 100) {
+    header.style.display = 'none';
+  } else {
+    // スクロール位置が一定以下の場合、ヘッダーを表示します。
+    header.style.display = 'block';
+  }
+});
+// スクロール時のイベントリスナーを追加します。
+window.addEventListener('scroll', () => {
+  // スクロール位置を取得します。
+  const scrollY = window.scrollY;
+
+  // 他の要素（例: メインコンテンツ）を取得します。
+  const mainContent = document.querySelector('.main-content');
+
+  // ヘッダーが特定のスクロール位置よりも下にスクロールした場合、
+  // メインコンテンツに特定のクラスを追加し、スタイルを変更します。
+  if (scrollY > 100) {
+    mainContent.classList.add('scrolled-content');
+  } else {
+    // スクロール位置が一定以下の場合、メインコンテンツからクラスを削除し、元のスタイルに戻します。
+    mainContent.classList.remove('scrolled-content');
+  }
+});  */
